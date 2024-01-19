a492f355511d51852aa250d6b69ec79ea791e43a
Marcelo León Cáceres <marcelo.leon.c@gmail.com>
2024-01-19 16:26:22 -0500

    491 Validating User Input and Outputting Validation Errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -56,7 +56,7 @@ const router = createBrowserRouter([
         ],
       },
       {
-        path: 'authentication',
+        path: 'auth',
         element: <AuthenticationPage />,
         action: authAction
       },
--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -23,7 +23,7 @@ export async function action({ request }) {
     password: data.get("password"),
   };
 
-  const response = fetch("http://localhost:8080/" + mode, {
+  const response = await fetch("http://localhost:8080/" + mode, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
