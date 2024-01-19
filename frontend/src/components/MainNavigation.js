b825ffacaf49119b7ae8cfda1d97cb885176bf92
Marcelo León Cáceres <marcelo.leon.c@gmail.com>
2024-01-19 14:45:00 -0500

    489 Working with Query Parameters

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -41,7 +41,7 @@ function MainNavigation() {
           </li>
           <li>
             <NavLink
-              to="/authentication"
+              to="/authentication?mode=login"
               className={({ isActive }) =>
                 isActive ? classes.active : undefined
               }
