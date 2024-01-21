335027a5552ea58c72ea935aa23cf63b9dba9dcd
MarceloLeonCaceres <marcelo.leon.c@gmail.com>
2024-01-20 21:39:40 -0500

    499 Managing the Token Expiration

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -44,6 +44,9 @@ export async function action({request}) {
   console.log('token en Authentication.js: ' + token);
 
   localStorage.setItem('token', token);
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours() + 1);
+  localStorage.setItem('expiration', expiration.toISOString());
 
   return redirect('/');
 }
