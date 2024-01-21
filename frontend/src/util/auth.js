a87306bd5f460201db6e58e9e72bf7a56e523ef7
MarceloLeonCaceres <marcelo.leon.c@gmail.com>
2024-01-20 21:22:20 -0500

    498 Adding Automatic Layout

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,19 +1,41 @@
-import { redirect } from "react-router-dom";
+import { redirect } from 'react-router-dom';
 
-export function getAuthToken(){
-    const token = localStorage.getItem('token');
-    return token;
+export function getTokenDuration() {
+  const storedExpirationDate = localStorage.getItem('expiration');
+  const expirationDate = new Date(storedExpirationDate);
+  const now = new Date();
+  const duration = expirationDate.getTime() - now.getTime();
+  return duration;
 }
 
-export function tokenLoader(){
-    return getAuthToken();
-}
-export function checkAuthLoader(){
-    const token = getAuthToken();
+export function getAuthToken() {
+  const token = localStorage.getItem('token');
 
-    if(!token){
-        return redirect('/auth');
-    }
-
+  if (!token) {
     return null;
+  }
+
+  const tokenDuration = getTokenDuration();
+
+  if (tokenDuration < 0) {
+    return 'EXPIRED';
+  }
+
+  return token;
 }
+
+export function tokenLoader() {
+  const token = getAuthToken();
+  return token;
+}
+
+export function checkAuthLoader() {
+  const token = getAuthToken();
+
+  if (!token) {
+    return redirect('/auth');
+  }
+  
+  return null;
+}
+
